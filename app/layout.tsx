acfe361ee97214a88af63aad09586c86ce9820c6
asad369 <shuzonsheikhbd@gmail.com>
2026-06-14 12:55:00 +0600

    Add Google Tag Manager scripts for enhanced tracking and analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -57,6 +57,20 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-M01GYZSS9V"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);} 
+              gtag('js', new Date());
+
+              gtag('config', 'G-M01GYZSS9V');
+            `,
+          }}
+        />
+      </head>
       <body className="min-h-full flex flex-col">
         {children}
         <SwRegister />
